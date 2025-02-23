05acdda6e0443bdf0fbb7f0cd534a4820f8009d0
somya-jangid <somyajangid78@gmail.com>
2025-02-24 00:30:27 +0530

    Update backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');const userSchema = new mongoose.Schema({ email: { type: String, required: true, unique: true }, password: { type: String, required: true }, name: String, subscriptionPlan: String, profiles: Array, createdAt: { type: Date, default: Date.now }, updatedAt: { type: Date, default: Date.now }});module.exports = mongoose.model('User', userSchema);
